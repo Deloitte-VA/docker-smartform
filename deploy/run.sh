625ca6129aa70fe3c6dace10e0be4d4b16c2ad96
Kun Yue <ykssky1@gmail.com>
2015-07-13 23:02:23 -0400

    SNPPROT-147 add script to dynamically config EyeCare code with host IP and custom port#

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo ${USER}
 echo ${DB_PORT_27017_TCP_ADDR}
-echo $DEPLOYED_CODE
+echo ${DEPLOYED_CODE}
+echo ${SMARTFORM_NODEJS_PORT}
+echo ${SMARTFORM_HOST_IP}
+echo ${CATALINA_HOME}
 
-# cp default.json.localhost default.json
-cp "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json.localhost "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json
-# sed -i "s/localhost/${USER}/g" default.json
-sed -i "s/localhost/${DB_PORT_27017_TCP_ADDR}/g" "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json
+# dynamically config EyeCare and ecurd
+cp "${CATALINA_HOME}"/webapps/EyeCare/app/js/services.js.localhost "${CATALINA_HOME}"/webapps/EyeCare/app/js/services.js&&sed -i "s/localhost:3001/${SMARTFORM_HOST_IP}:${SMARTFORM_NODEJS_PORT}/g" "${CATALINA_HOME}"/webapps/EyeCare/app/js/services.js&&cp "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json.localhost "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json&&sed -i "s/localhost/${DB_PORT_27017_TCP_ADDR}/g" "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json
 
 cd "$DEPLOYED_CODE"/vlerdas-ecrud
 catalina.sh run & node ./bin/server.js
