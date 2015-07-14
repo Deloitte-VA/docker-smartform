865bd038db363546a730bf5d2ea2c1caccdf4390
Kun Yue <ykssky1@gmail.com>
2015-07-14 14:00:13 -0400

    SNPPROT-147 fixed for EyeCare revision deleted by tomcat automatically

--- deploy/run.sh
+++ deploy/run.sh
@@ -6,8 +6,20 @@ echo ${SMARTFORM_NODEJS_PORT}
 echo ${SMARTFORM_HOST_IP}
 echo ${CATALINA_HOME}
 
+if [ ! -d "${DEPLOYED_CODE}"/EyeCare ]; then
+    unzip "${DEPLOYED_CODE}"/webapps.zip -d "${DEPLOYED_CODE}"
+fi
+
 # dynamically config EyeCare and ecurd
-cp "${CATALINA_HOME}"/webapps/EyeCare/app/js/services.js.localhost "${CATALINA_HOME}"/webapps/EyeCare/app/js/services.js&&sed -i "s/localhost:3001/${SMARTFORM_HOST_IP}:${SMARTFORM_NODEJS_PORT}/g" "${CATALINA_HOME}"/webapps/EyeCare/app/js/services.js&&cp "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json.localhost "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json&&sed -i "s/localhost/${DB_PORT_27017_TCP_ADDR}/g" "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json
+cp "${DEPLOYED_CODE}"/EyeCare/app/js/services.js.localhost "${DEPLOYED_CODE}"/EyeCare/app/js/services.js
+sed -i "s/localhost:3001/${SMARTFORM_HOST_IP}:${SMARTFORM_NODEJS_PORT}/g" "${DEPLOYED_CODE}"/EyeCare/app/js/services.js
+
+rm -rf "$CATALINA_HOME"/webapps/EyeCare*
+cd "${DEPLOYED_CODE}"/EyeCare
+jar cvf "$CATALINA_HOME"/webapps/EyeCare.war .
+cp -r "${DEPLOYED_CODE}"/EyeCare "$CATALINA_HOME"/webapps/
+
+cp "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json.localhost "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json&&sed -i "s/localhost/${DB_PORT_27017_TCP_ADDR}/g" "$DEPLOYED_CODE"/vlerdas-ecrud/config/default.json
 
 cd "$DEPLOYED_CODE"/vlerdas-ecrud
 catalina.sh run & node ./bin/server.js
